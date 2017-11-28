d52c0cad62b5b72c59319561fe6c125940cf0a46
m43stlin <ws.brndn@yahoo.com>
2017-11-27 22:32:30 -0500

    Created a header file for functions that will deal with parsing data for the database table.

---
+++ headers/tble_funcs.h
@@ -0,0 +1,9 @@
+#ifndef POSTGRES_CSV_LOADER_TBLE_FUNCS_H
+#define POSTGRES_CSV_LOADER_TBLE_FUNCS_H
+
+#include <stdlib.h>
+#include <memory.h>
+
+char** getColNames(char* colNames);
+
+#endif //POSTGRES_CSV_LOADER_TBLE_FUNCS_H
